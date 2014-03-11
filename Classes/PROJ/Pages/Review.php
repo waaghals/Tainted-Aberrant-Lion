61aeacc9e4b10f7b52e61b124888222693d09f28
Yorick666 <yorickvanklinken@gmail.com>
2014-03-11 15:15:40 +0100

    Login fix, review page gemaakt en begin van review center

--- Classes/PROJ/Pages/Review.php
+++ Classes/PROJ/Pages/Review.php
@@ -7,11 +7,16 @@ class Review extends MainPage {
     public function getContent() {
         $review = new \PROJ\View\Review;
 
-        if(isset($_POST['reviewButton'])) {
-            //Submit button pressed
-            
+        if (isset($_POST['reviewButton'])) {
+
+            $em = \PROJ\Helper\DoctrineHelper::instance()->getEntityManager();
+            $newReview = new \PROJ\Entities\Review();
+            $newReview->setText($_POST['review']);
+            $newReview->setRating($_POST['rating']);
+            $em->persist($newReview);
+            $em->flush();
         }
-        
+
         $r = $review->getContent();
 
         return $r;
