468272bbac5f94a879346fe8baf4661cef5e8b0b
Waaghals <patrick.stroef@gmail.com>
2014-03-11 15:15:33 +0100

    Moved AccountController to AccountService removed old files.

--- Classes/PROJ/Services/AccountService.php
+++ Classes/PROJ/Services/AccountService.php
@@ -1,41 +1,32 @@
 <?php
 
-namespace PROJ\Controller;
+namespace PROJ\Services;
 
-class AccountController {
-
-    private static $instance;
-
-    /**
-     * @return \PROJ\Controller\AccountController
-     */
-    public static function instance() {
-        if (self::$instance == null) {
-            self::$instance = new self();
-        }
-
-        return self::$instance;
-    }
+class AccountService {
 
     public function checkLoginCredentials($username, $password) {
         $em = \PROJ\Helper\DoctrineHelper::instance()->getEntityManager();
         $user = $em->getRepository('PROJ\Entities\Account')->findBy(array('username' => $username));
-        if($user != null) {
+        if ($user != null) {
             //Username exists
             $passwordEntered = hash('sha512', $password . $user->getSalt());
-            
-            if($passwordEntered == $user->getPassword())    //Login correct
+
+            if ($passwordEntered == $user->getPassword())    //Login correct
                 return true;
         }
         return false;
     }
-    
+
     public function doLogin($username) {
         $em = \PROJ\Helper\DoctrineHelper::instance()->getEntityManager();
         $user = $em->getRepository('PROJ\Entities\Account')->findBy(array('username' => $username));
-        if($user != null) { //Just to be sure
+        if ($user != null) { //Just to be sure
             $_SESSION['user'] = $user;
         }
+    }
+    
+    public static function isLoggedIn() {
+        //TODO
     }
 
 }
