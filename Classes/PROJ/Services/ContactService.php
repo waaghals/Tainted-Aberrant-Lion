7ab4b9c7b0cf5f39cf15d9c9e0fc1a8a001c4e84
Waaghals <patrick.stroef@gmail.com>
2014-03-17 11:31:19 +0100

    Merged the contact with the new mvc architecture and added some checks

---
+++ Classes/PROJ/Services/ContactService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace PROJ\Services;
+
+use PROJ\Exceptions\ServerException;
+
+/**
+ * Description of ContactformController
+ *
+ * @author Sam
+ */
+class ContactService {
+
+    public function sendMail($toEmail) {
+        mail($toEmail, $_POST['subject'], filter_var($_POST['mailContent'], FILTER_SANITIZE_FULL_SPECIAL_CHARS), 'From: ' . $_POST['mailFrom']);
+    }
+
+    public function getStudentById($studentId) {
+        if (!is_int(intval($studentId))) {
+            $msg = sprintf("StudentId with value: %s is not a valid id.", $studentId);
+            throw new ServerException($msg, ServerException::SERVER_ERROR);
+        }
+
+        $em = \PROJ\Helper\DoctrineHelper::instance()->getEntityManager();
+        $student = $em->getRepository('/PROJ/Entities/Student')->find($studentId);
+
+        if (!is_a($student, "Student")) {
+            $msg = "A unexpected object was returned from the database.";
+            throw new ServerException($msg, ServerException::SERVER_ERROR);
+        }
+
+        if ($student == null) {
+            $msg = sprintf("A student with id: %s does not exists in the database.", $studentId);
+            throw new ServerException($msg, ServerException::NOT_FOUND);
+        }
+
+        //Student is valid
+        return $student;
+    }
+
+}
