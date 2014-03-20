a1adf97b4a91902a0b688c33abcd0b0ee2462a0f
tojba <yougotspam123@gmail.com>
2014-03-20 17:00:11 +0100

    Fixed test

--- tests/RegistrationTest.php
+++ tests/RegistrationTest.php
@@ -1,53 +1,78 @@
 <?php
 
 namespace PROJ\Tests;
-
 use PROJ\Services\AccountService;
 
-class RegistrationTest extends \PHPUnit_Framework_TestCase {
+class RegistrationTest extends \PHPUnit_Framework_TestCase
+{
 
-    public $dummydata;
+    public $dummydata = array();
     public $service;
 
-    public function __construct() {
-       // $this->ResetData();
-        $$this->service = new AccountService();
+    public function __construct()
+    {
+        // $this->ResetData();
+        $this->service = new AccountService();
     }
 
-    public function ResetData() {
+    public function setUp()
+    {
         $this->dummydata['username'] = "Dummy";
         $this->dummydata['password'] = "Dummy";
         $this->dummydata['passwordagain'] = "Dummy";
         $this->dummydata['firstname'] = "Dummy";
         $this->dummydata['surname'] = "Dummy";
         $this->dummydata['city'] = "Dummy";
+        $this->dummydata['email'] = "Dummy@example.com";
         $this->dummydata['street'] = "Dummy";
         $this->dummydata['zipcode'] = "Dummy";
+        $this->dummydata['streetnumber'] = 12;
     }
 
-    public function testRegistrationInputValidationCheckPass() {
-        $this->ResetData();
-
-        $this->assertEquals($$this->service->validateInput($this->dummydata), true);
+    public function testRegistrationInputValidationCheckPass()
+    {
+        $this->assertTrue($this->service->validateInput($this->dummydata));
     }
 
-    public function testRegistraionInputValidationUsernameFail() {
-        $this->ResetData();
-
+    public function testRegistraionInputValidationUsernameFail()
+    {
         $this->dummydata['username'] = "<script>alert('hoi');</script>";
 
-        $this->assertNotEquals($$this->service->validateInput($this->dummydata), true);
+        $this->assertTrue($this->service->validateInput($this->dummydata));
     }
 
-    public function testRegistraionInputValidationPasswordNotEquals() {
-        $this->ResetData();
-
+    public function testRegistraionInputValidationPasswordNotEquals()
+    {
         $this->dummydata['passwordagain'] = "NotDummy";
 
-        $this->assertNotEquals($$this->service->validateInput($this->dummydata), true);
+        $this->assertNotEquals($this->service->validateInput($this->dummydata), true);
     }
-    
-    public function __toString() {
+
+    public function RegistraionInputValidationInputLengthFail()
+    {
+        $this->ResetData();
+
+        $dummydata['username'] = "";
+        // string length = 321
+        $dummydata['firstname'] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        $this->assertNotEquals($this->service->validate_input($dummydata), true);
+    }
+
+    public function RegistraionInputValidationNoInput()
+    {
+        $this->ResetData();
+
+        foreach ($dummydata as $data)
+            $data = "";
+
+        $this->assertNotEquals($this->service->validate_input($dummydata), true);
+    }
+
+    public function __toString()
+    {
         return "";
     }
+
 }
+    
