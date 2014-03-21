057e98db68d6968000f444c3682484b00c80b915
Yorick666 <yorickvanklinken@gmail.com>
2014-03-21 23:43:21 +0100

    #21 Review is af zonder responsive, want het ziet er nog niet uit. iemand die me wil helpen?

--- Classes/PROJ/Controllers/ReviewController.php
+++ Classes/PROJ/Controllers/ReviewController.php
@@ -2,27 +2,51 @@
 
 namespace PROJ\Controllers;
 
-use PROJ\Helper\HeaderHelper;
 use PROJ\Tools\Template;
+use PROJ\Services\AccountService;
+use PROJ\Services\ReviewService;
+use PROJ\Helper\HeaderHelper;
 
 class ReviewController extends BaseController {
 
-    public function IndexAction() {
-       $t = new Template("ReviewCenter");
-        echo $t; 
+    private $reviewService;
+    private $accountService;
+
+    public function __construct() {
+        $this->accountService = new AccountService();
+        $this->accountService->doLogin("admin");
+
+        if (!$this->accountService->isLoggedIn())
+        #hier komt een error, maar niemand reageerde op vraag :P
+            HeaderHelper::redirect();
+
+        $this->reviewService = new ReviewService();
     }
 
-    public function AddAction() {
+    public function IndexAction() {
+        $t = new Template("ReviewCenter");
+        $t->projects = $this->reviewService->loadReviewCenter();
+        echo $t;
+    }
+
+    public function AddAction($ProjectId) {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $this->reviewService->addReview($_POST);
+        }
         $t = new Template("Review");
+        $t->id = $ProjectId;
         $t->text = "";
         echo $t;
     }
 
     public function EditAction($ReviewId) {
-
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $this->reviewService->editReview($ReviewId, $_POST);
+        }
+        $review = $this->reviewService->getReview($ReviewId);
         $t = new Template("Review");
-        $t->text = "hallo";
-        $t->grade = 3;
+        $t->text = (string) $review->gettext();
+        $t->grade = (int) $review->getrating();
         echo $t;
     }
 
