ddaedc470170dc83252f5950ff022f1809cf725b
tojba <yougotspam123@gmail.com>
2014-03-22 21:10:45 +0100

    Fixed search no result message

--- Classes/PROJ/Controllers/SearchController.php
+++ Classes/PROJ/Controllers/SearchController.php
@@ -5,13 +5,13 @@ namespace PROJ\Controllers;
 class SearchController extends BaseController {
 
     function IndexAction() {
-        $searchresult = $_SESSION['searchresult'];
+
         $html = null;
         $html .= "Zoekresultaten:";
-        if (empty($searchresult)) {
+        if (empty($_SESSION['searchresult'])) {
             $html .= "<p>Er zijn geen reviews gevonden.</p>";
         }
-        foreach ($searchresult as $value) {
+        foreach ($_SESSION['searchresult'] as $value) {
             $html .= "<p>Instituut naam: " . $value['institutename'] . "<br> Long: " . $value['long'] . "<br> Lat: " . $value['lat'] . "<br> Review: " . $value['text'] . "<br> Rating: " . $value['rating'] . "<br>Review door: " . $value['studentname'] . " " . $value['studentsurname'] . "<br>Email: " . $value['email'] . "</p>";
         }
         echo $html;
