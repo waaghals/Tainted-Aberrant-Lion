4c75bc09ea6cb4c195f02ff0493a30e5e2ed7729
Jelle van es <jellevanes@hotmail.com>
2014-03-23 16:15:12 +0100

    bugfix searchcontroller

--- Classes/PROJ/Controllers/SearchController.php
+++ Classes/PROJ/Controllers/SearchController.php
@@ -5,16 +5,18 @@ namespace PROJ\Controllers;
 class SearchController extends BaseController {
 
     function IndexAction() {
-        $searchresult = $_SESSION['searchresult'];
-        $html = null;
-        $html .= "Zoekresultaten:";
-        if (empty($searchresult)) {
-            $html .= "<p>Er zijn geen reviews gevonden.</p>";
+        if (!empty($_SESSION['searchresult'])) {
+            $searchresult = $_SESSION['searchresult'];
+            $html = null;
+            $html .= "Zoekresultaten:";
+            if (empty($searchresult)) {
+                $html .= "<p>Er zijn geen reviews gevonden.</p>";
+            }
+            foreach ($searchresult as $value) {
+                $html .= "<p>Instituut naam: " . $value['institutename'] . "<br> Long: " . $value['long'] . "<br> Lat: " . $value['lat'] . "<br> Review: " . $value['text'] . "<br> Rating: " . $value['rating'] . "<br>Review door: " . $value['studentname'] . " " . $value['studentsurname'] . "<br>Email: " . $value['email'] . "</p>";
+            }
+            echo $html;
         }
-        foreach ($searchresult as $value) {
-            $html .= "<p>Instituut naam: " . $value['institutename'] . "<br> Long: " . $value['long'] . "<br> Lat: " . $value['lat'] . "<br> Review: " . $value['text'] . "<br> Rating: " . $value['rating'] . "<br>Review door: " . $value['studentname'] . " " . $value['studentsurname'] . "<br>Email: " . $value['email'] . "</p>";
-        }
-        echo $html;
     }
 
 }
