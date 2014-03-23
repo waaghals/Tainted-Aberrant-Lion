6f0d99f67925b5533bae36c59ffb31d04e1aaa40
Koozza <thijs@dalmaijer.net>
2014-03-23 16:02:28 +0100

    no message

---
+++ Classes/PROJ/Entities/LoginAttempt.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace PROJ\Entities;
+
+/**
+ * @Entity 
+ */
+class Account {
+
+    /**
+     * @Id @Column(type="integer")
+     * @GeneratedValue
+     */
+    private $id;
+
+    /**
+     * @Column(type="string", unique=true)
+     */
+    private $username;
+
+    /**
+     * @Column(type="string")
+     */
+    private $password;
+    
+    /**
+     * @Column(type="string")
+     */
+    private $salt;    
+    
+    /**
+     * @OneToOne(targetEntity="\PROJ\Entities\Student", mappedBy="account")
+     */
+    private $student;
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getUsername() {
+        return $this->username;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function setUsername($username) {
+        $this->username = $username;
+    }
+
+    public function setPassword($password) {
+        $this->password = $password;
+    }
+    public function getSalt() {
+        return $this->salt;
+    }
+
+    public function setSalt($salt) {
+        $this->salt = $salt;
+    }
+}
+
+?>
