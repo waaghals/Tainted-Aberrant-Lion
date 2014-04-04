db4716adf3a8ff3ee6e7282399a3815ef7e48c7e
Koozza <thijs@dalmaijer.net>
2014-04-04 15:19:58 +0200

    * Fix Management->Home style * Management -> Change password done

--- Classes/PROJ/Controllers/ManagementController.php
+++ Classes/PROJ/Controllers/ManagementController.php
@@ -16,8 +16,21 @@ class ManagementController extends BaseController {
         $this->serveManagementTemplate();
     }
     
-    public function accountAction() {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {    //Save account details if possible
+    public function ChangePasswordAction() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {    //Save account details
+            $valid = $this->validate_ChangePassword();
+            if($valid === "succes") {
+                $this->additionalVals = array('error'=>'Change password succesfully.');
+            }else{
+                $this->additionalVals = array('error'=>$valid);
+            }
+        }
+        $this->page = "ChangePassword";
+        $this->serveManagementTemplate();
+    }
+    
+    public function MyAccountAction() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {    //Save account details
             $valid = $this->validate_input($_POST);
             if($valid === "succes"){
                 $em = \PROJ\Helper\DoctrineHelper::instance()->getEntityManager();
@@ -37,7 +50,7 @@ class ManagementController extends BaseController {
                 $this->additionalVals = array('error'=>$valid);
             }
         }
-        $this->page = "Account";
+        $this->page = "MyAccount";
         $this->serveManagementTemplate();
     }
     
@@ -71,6 +84,36 @@ class ManagementController extends BaseController {
                 return "House number is not a number";
         
         return "succes";
+    }
+    
+    private function validate_ChangePassword() {
+        //Get current user
+        $em = \PROJ\Helper\DoctrineHelper::instance()->getEntityManager();
+        $user = $em->getRepository('\PROJ\Entities\Account')->find($_SESSION['userID']);
+        
+        //Valdidate old password
+        $passwordEnteredOld = hash('sha512', $_POST['old_password'] . $user->getSalt());
+        if($passwordEnteredOld == $user->getPassword()) {
+            if($_POST['old_password'] != $_POST['new_password']) {
+                if($_POST['new_password'] == $_POST['rep_new_password']) {
+                    $passwordEnteredNew = hash('sha512', $_POST['new_password'] . $user->getSalt());
+                    $user->setPassword($passwordEnteredNew);
+                    $em->persist($user);
+                    $em->flush();
+                    
+                    //Change session to prevent logout
+                    $_SESSION['login_string'] = hash('sha512', $user->getPassword() . $_SERVER['HTTP_USER_AGENT'] . $_SERVER['REMOTE_ADDR']);
+                    
+                    return "succes";
+                }else{
+                    return "New passwords didn't match.";
+                }
+            }else{
+                return "New password can't be the same as the old password.";
+            }
+        }else{
+            return "Old password didn't match.";
+        }
     }
 
 }
