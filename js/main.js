e025191d177a2faf73fbc665b104f6779160d23f
Koozza <thijs@dalmaijer.net>
2014-04-22 01:10:29 +0200

    * Blackout Content Verplaatst * Remove scherm voor Locations af kwa design

--- js/main.js
+++ js/main.js
@@ -14,72 +14,75 @@ $(document).ready(function() {
     });
     $('.mylocation_add').click(function() {
         $('#blackout').fadeIn();
-        $('#blackout_create_review').fadeOut();
-        $('#blackout_create_project').fadeOut();
+        $('[id^=blackout_]:visible').fadeOut();
         $('#blackout_create_location').fadeIn();
     });
-    
+
+    $('.mylocation_remove').click(function() {
+        $('#blackout').fadeIn();
+        $('[id^=blackout_]:visible').fadeOut();
+        $('#blackout_delete_location').fadeIn();
+    });
+
     $('.myprojects_add').click(function() {
         $('#blackout').fadeIn();
-        $('#blackout_create_review').fadeOut();
-        $('#blackout_create_location').fadeOut();
+        $('[id^=blackout_]:visible').fadeOut();
         $('#blackout_create_project').fadeIn();
     });
 
     $('.myreviews_add').click(function() {
         $('#blackout').fadeIn();
-        $('#blackout_create_project').fadeOut();
-        $('#blackout_create_location').fadeOut();
+        $('[id^=blackout_]:visible').fadeOut();
         $('#blackout_create_review').fadeIn();
     });
-    
+
     $('#blackout').click(function(e) {
-        if($(event.target).is($('#blackout'))) {
+        if ($(event.target).is($('#blackout'))) {
             $('#blackout').fadeOut(function() {
                 $(this).children().hide();
             });
-        }else{
-            e.preventDefault(); 
+        } else {
+            e.preventDefault();
         }
     });
-    
+
     $('#create_location').click(function() {
         $(this).hide();
-        $.post( "/Ajax/CreateLocation", $( "[name='create_location_form']" ).serialize(), function(data) {
-            if(data == "succes") {
+        $.post("/Ajax/CreateLocation", $("[name='create_location_form']").serialize(), function(data) {
+            if (data == "succes") {
                 $('#blackout').fadeOut(function() {
                     location.reload();
                 });
-            }else{
+            } else {
                 $('#create_location').show();
                 $('#addLocationError').show().children().first().html(data);
             }
         });
     });
-    
+
     $('#create_project').click(function() {
         $(this).hide();
-        $.post( "/Ajax/CreateProject", $( "[name='create_project_form']" ).serialize(), function(data) {
-            if(data == "succes") {
+        $.post("/Ajax/CreateProject", $("[name='create_project_form']").serialize(), function(data) {
+            if (data == "succes") {
                 $('#blackout').fadeOut(function() {
                     location.reload();
                 });
-            }else{
+            } else {
                 $('#create_project').show();
                 $('#addProjectError').show().children().first().html(data);
             }
         });
     });
-    
+
     $('#create_review').click(function() {
         $(this).hide();
-        $.post( "/Ajax/CreateReview", $( "[name='create_review_form']" ).serialize(), function(data) {
+        $.post("/Ajax/CreateReview", $("[name='create_review_form']").serialize(), function(data) {
             console.log(data);
-            if(data == "succes") {
+            if (data == "succes") {
                 $('#blackout').fadeOut(function() {
                     location.reload();
                 });
-            }else{
+            } else {
                 $('#create_review').show();
                 $('#addProjectError').show().children().first().html(data);
             }
@@ -88,13 +91,13 @@ $(document).ready(function() {
 });
 
 $(document).keypress(function(e) {
-    if(e.which == 13) {
-        if($('#register_container').length !== 0)
-            if($('#blackout').length === 0)
+    if (e.which == 13) {
+        if ($('#register_container').length !== 0)
+            if ($('#blackout').length === 0)
                 $('#register_container').children()[0].submit();
-        if($('#login_container').length !== 0)
+        if ($('#login_container').length !== 0)
             $('#login_container').children()[0].submit();
-        if($('#contact_container').length !== 0)
+        if ($('#contact_container').length !== 0)
             $('#contact_container').children()[0].submit();
     }
 });
