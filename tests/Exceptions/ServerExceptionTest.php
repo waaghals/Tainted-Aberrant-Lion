bbbf62545fcf6b63de246de2f991db85eea181e7
Patrick Berenschot <patrick.stroef@gmail.com>
2014-06-06 19:43:06 +0200

    Fix ServerException in Request

--- tests/Exceptions/ServerExceptionTest.php
+++ tests/Exceptions/ServerExceptionTest.php
@@ -1,18 +1,24 @@
 <?php
+
 namespace Tests\Exceptions;
+
 use PROJ\Exceptions\ServerException;
+
 /**
  * Description of ServerExceptionTest
  *
  * @author Patrick
  */
-class ServerExceptionTest extends \PHPUnit_Framework_TestCase {
-    
+class ServerExceptionTest extends \PHPUnit_Framework_TestCase
+{
+
     /**
      * @expectedException \Exception
      * @expectedExceptionMessage Not a valid status code for ServerException.
      */
-    public function testWrongExceptionCode() {
+    public function testWrongExceptionCode()
+    {
         throw new ServerException("Example server exception.", -1);
     }
+
 }
