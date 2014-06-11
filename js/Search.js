54c28dd50265845254da2a25027ed21774c4acb2
Juuouji <samsam_31393@hotmail.com>
2014-06-11 16:40:43 +0200

    ID toegevoegd aan de divjes bij het zoeken, dit wordt gebruikt voor Selenium

--- js/Search.js
+++ js/Search.js
@@ -20,7 +20,7 @@ function lookup(key_count) {
             $.each(data, function(i, result) {
                 //Review hit
                 if (result['matchedOn'] === "review") {
-                    var newelement = $("<div style='cursor:pointer;' onclick='fakeMarkerClick(" + result['instituteid'] + "," + result['lat'] + "," + result['lng'] + "); loadReview(" + result['revid'] + ");'></div>").appendTo('#searchresult');
+                    var newelement = $("<div id='record" + (i + 1) + "' style='cursor:pointer;' onclick='fakeMarkerClick(" + result['instituteid'] + "," + result['lat'] + "," + result['lng'] + "); loadReview(" + result['revid'] + ");'></div>").appendTo('#searchresult');
                     newelement.append("<p style='font-weight:bold; margin-bottom:3px;'>Review by " + result['studentname'] + " " + result['studentsurname'] + "</p>");
                     newelement.append("<p>" + result['text'] + "</p>");
                     if (i !== data.length - 1) {
@@ -30,7 +30,7 @@ function lookup(key_count) {
 
                 //Institute hit
                 if (result['matchedOn'] === "instname") {
-                    var newelement = $("<div style='cursor:pointer;' onclick='fakeMarkerClick(" + result['instituteid'] + "," + result['lat'] + "," + result['lng'] + ")'></div>").appendTo('#searchresult');
+                    var newelement = $("<div id='record" + (i + 1) + "' style='cursor:pointer;' onclick='fakeMarkerClick(" + result['instituteid'] + "," + result['lat'] + "," + result['lng'] + ")'></div>").appendTo('#searchresult');
                     newelement.append("<p style='font-weight:bold; margin-bottom:3px;'> " + result['institutename'] + "</p>");
                     newelement.append("<p>" + result['instituteplace'] + "</p>");
                     if (i !== data.length - 1) {
