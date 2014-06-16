b043b7b050961238fc4e4d222731acd013c9e789
Patrick Berenschot <patrick.stroef@gmail.com>
2014-06-16 22:16:36 +0200

    Fix registration test

--- tests/RegistrationTest.php
+++ tests/RegistrationTest.php
@@ -18,21 +18,24 @@ class RegistrationTest extends \PHPUnit_Framework_TestCase
 
     public function ResetData()
     {
-        $this->dummydata['username'] = "Dummy";
-        $this->dummydata['password'] = "Dummy";
-        $this->dummydata['passwordagain'] = "Dummy";
-        $this->dummydata['firstname'] = "Dummy";
-        $this->dummydata['surname'] = "Dummy";
-        $this->dummydata['city'] = "Dummy";
-        $this->dummydata['street'] = "Dummy";
-        $this->dummydata['zipcode'] = "Dummy";
+        $this->dummydata['username']         = "Patrick";
+        $this->dummydata['password']         = "test";
+        $this->dummydata['passwordagain']    = "test";
+        $this->dummydata['firstname']        = "Patrick";
+        $this->dummydata['surname']          = "Berenschot";
+        $this->dummydata['city']             = "'s -Hertogenbosch";
+        $this->dummydata['street']           = "Straatnaam";
+        $this->dummydata['zipcode']          = "5111AA";
+        $this->dummydata['streetnumber']     = "256";
+        $this->dummydata['registrationcode'] = "abc";
     }
 
     public function testRegistrationInputValidationCheckPass()
     {
         $this->ResetData();
 
-        $this->assertEquals($this->service->validateInput($this->dummydata), true);
+        $this->assertEquals($this->service->validateInput($this->dummydata),
+                                                          true);
     }
 
     public function testRegistrationInputValidationUsernameFail()
@@ -41,7 +44,8 @@ class RegistrationTest extends \PHPUnit_Framework_TestCase
 
         $this->dummydata['username'] = "<script>alert('hoi');</script>";
 
-        $this->assertNotEquals($this->service->validateInput($this->dummydata), true);
+        $this->assertNotEquals($this->service->validateInput($this->dummydata),
+                                                             true);
     }
 
     public function testRegistrationInputValidationPasswordNotEquals()
@@ -50,7 +54,8 @@ class RegistrationTest extends \PHPUnit_Framework_TestCase
 
         $this->dummydata['passwordagain'] = "NotDummy";
 
-        $this->assertNotEquals($this->service->validateInput($this->dummydata), true);
+        $this->assertNotEquals($this->service->validateInput($this->dummydata),
+                                                             true);
     }
 
     public function __toString()
